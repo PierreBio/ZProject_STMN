b44e9835b2a0034c16122abe8225461443ba3ca1
Nelyanen <dreamer.chrysalis@gmail.com>
2020-09-17 16:45:34 +0200

    Health an Score tracking + GUI

---
+++ Assets/PlayerFunctions/Scripts/HealthBarHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHandler : MonoBehaviour
+{
+    private static Image HealthBarImage;
+
+    /// <summary>
+    /// Sets the health bar value
+    /// </summary>
+    /// <param name="value">should be between 0 to 1</param>
+    public static void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if (HealthBarImage.fillAmount < 0.2f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if (HealthBarImage.fillAmount < 0.4f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+
+    public static float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="healthColor">Color </param>
+    public static void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    internal void setHealthValue(float value, float maxvalue)
+    {
+        float newValue = value / maxvalue;
+        SetHealthBarValue(newValue);
+    }
+
+    /// <summary>
+    /// Initialize the variable
+    /// </summary>
+    private void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+    }
+}

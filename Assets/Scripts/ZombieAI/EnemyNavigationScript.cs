f023239d4fc63e1bc9de170d1e8dc7a9617dd52c
Pierre <pi.billaud@gmail.com>
2020-09-17 16:57:12 +0200

    AI Script improved
    AI script improved (WIP to add animations)

--- Assets/Scripts/ZombieAI/EnemyNavigationScript.cs
+++ Assets/Scripts/ZombieAI/EnemyNavigationScript.cs
@@ -7,6 +7,12 @@ public class EnemyNavigationScript : MonoBehaviour
 {
 
     NavMeshAgent nm;
+    public enum AIState { attacking, chasing };
+    Animator m_Animator;
+
+    [Header("AI settings")]
+    public AIState aiState = AIState.chasing;
+    public float distanceAttack = 10f;
 
     [Header("Target settings")]
     public Transform target;
@@ -14,12 +20,52 @@ public class EnemyNavigationScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        m_Animator = gameObject.GetComponent<Animator>();
         nm = GetComponent<NavMeshAgent>();
+        StartCoroutine(Think());
     }
 
     // Update is called once per frame
     void Update()
     {
-        nm.SetDestination(target.position);
+
+    }
+
+    IEnumerator Think()
+    {
+        while(true)
+        {
+            switch(aiState)
+            {
+                case AIState.attacking:
+
+                    float dist = Vector3.Distance(target.position, transform.position);
+
+                    if (dist >= distanceAttack)
+                    {
+                        m_Animator.SetTrigger("Z_Run");
+                        aiState = AIState.chasing;
+                    }
+
+                    nm.SetDestination(transform.position);
+                    break;
+                case AIState.chasing:
+
+                    dist = Vector3.Distance(target.position, transform.position);
+
+                    if (dist < distanceAttack)
+                    {
+                        m_Animator.SetTrigger("Z_Attack");
+                        aiState = AIState.attacking;
+                    }
+
+                    nm.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 }

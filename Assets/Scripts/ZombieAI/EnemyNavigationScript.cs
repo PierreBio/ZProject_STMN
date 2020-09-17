27f15d3f37f5845ba3a628c82bbaf32bf5b8fa60
Pierre <pi.billaud@gmail.com>
2020-09-17 15:45:21 +0200

    AI Scene setup
    Basic AI script + basic setup environment

---
+++ Assets/Scripts/ZombieAI/EnemyNavigationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigationScript : MonoBehaviour
+{
+
+    NavMeshAgent nm;
+
+    [Header("Target settings")]
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nm.SetDestination(target.position);
+    }
+}
